f931f2502772d7647161fbe0a0fb4dda969e1ac4
Mojtaba Arezoomand <mojtaba.arezoomand@snapp.cab>
2023-01-02 14:28:39 +0330

    feat: add wraper funcs for http.Handler and http.HandlerFunc

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,8 @@
 package kid
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
@@ -28,4 +30,41 @@ func TestGetPath(t *testing.T) {
 
 	assert.NotEmpty(t, u.RawPath)
 	assert.Equal(t, u.RawPath, getPath(u))
+}
+
+func TestWrapHandler(t *testing.T) {
+	k1 := New()
+	k2 := New()
+
+	k2.GET("/test", func(c *Context) error {
+		return c.JSONByte(http.StatusOK, []byte(`{"status": "ok"}`))
+	})
+
+	k1.GET("/test", WrapHandler(k2))
+
+	req := httptest.NewRequest(http.MethodGet, "/test", nil)
+	res := httptest.NewRecorder()
+
+	k1.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, `{"status": "ok"}`, res.Body.String())
+}
+
+func TestWrapHandlerFunc(t *testing.T) {
+	k := New()
+
+	k.GET("/test", WrapHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(`{"status": "ok"}`))
+		assert.NoError(t, err)
+	}))
+
+	req := httptest.NewRequest(http.MethodGet, "/test", nil)
+	res := httptest.NewRecorder()
+
+	k.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Equal(t, `{"status": "ok"}`, res.Body.String())
 }
