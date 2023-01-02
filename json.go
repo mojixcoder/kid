980e7405792116566503999bcd83fe40e0631f75
Mojtaba Arezoomand <mojtaba.arezoomand@snapp.cab>
2023-01-02 14:54:47 +0330

    fix: fix handling errors when reading json from request body

--- json.go
+++ json.go
@@ -2,7 +2,6 @@ package kid
 
 import (
 	"encoding/json"
-	"io"
 	"net/http"
 )
 
@@ -37,17 +36,11 @@ func (s defaultJSONSerializer) Write(c *Context, in any, indent string) error {
 
 // Unmarshal reads request's body as JSON and puts it in the given obj.
 func (s defaultJSONSerializer) Read(c *Context, out any) error {
-	err := json.NewDecoder(c.Request().Body).Decode(out)
-
-	if _, ok := err.(*json.SyntaxError); ok {
+	if err := json.NewDecoder(c.Request().Body).Decode(out); err != nil {
+		if _, ok := err.(*json.InvalidUnmarshalError); ok {
+			return NewHTTPError(http.StatusInternalServerError).WithMessage(err.Error()).WithError(err)
+		}
 		return NewHTTPError(http.StatusBadRequest).WithMessage(err.Error()).WithError(err)
-	} else if _, ok := err.(*json.UnmarshalTypeError); ok {
-		return NewHTTPError(http.StatusBadRequest).WithMessage(err.Error()).WithError(err)
-	} else if err == io.ErrUnexpectedEOF {
-		return NewHTTPError(http.StatusBadRequest).WithMessage(err.Error()).WithError(err)
-	} else if err != nil {
-		return NewHTTPError(http.StatusInternalServerError).WithMessage(err.Error()).WithError(err)
 	}
-
-	return err
+	return nil
 }
